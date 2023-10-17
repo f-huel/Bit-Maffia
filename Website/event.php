3253cfa84b6c38c677fd891b4dc23458b06caf40
Felix <felix.huel6@gmail.com>
2023-10-17 15:52:17 +0200

    all pages have navbar + styling

---
+++ Website/event.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BIT FCM | HOME</title>
+    <link rel="stylesheet" href="css/event.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="images/bit-fcm.png">
+</head>
+<body>
+
+<nav class="stroke navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+
+            <a class="navbar-brand" href="index.php"><img src="images/bit-fcm.png" alt="" width="140" height="140"></a>
+
+            <button class="navbar-toggler d-flex d-lg-none flex-column justify-content-around" type="button"
+                data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true"
+                aria-label="Toggle navigation">
+                <span class="toggler-icon top-bar"></span>
+                <span class="toggler-icon middle-bar"></span>
+                <span class="toggler-icon bottom-bar"></span>
+            </button>
+
+            <div class="navbar-collapse collapse show float-right" id="navbarNav">
+                <ul class="d-inline navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a href="event.php">EVENT</a></li>
+                    <li class="nav-item"><a href="ideas.php">IDEAS</a></li>
+                    <li class="nav-item"><a href="contact.php">CONTACT</a></li>
+                    <li class="nav-item"><a href="team.php">TEAM</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
+        crossorigin="anonymous"></script>
+    
+</body>
+</html>
