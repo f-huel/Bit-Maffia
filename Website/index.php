bd13457f5c003c81ab95e0e2d38cf5f5f6042fa5
Felix <felix.huel6@gmail.com>
2023-10-21 23:53:38 +0200

    new site idea, different layout, new styling, javascript usage in navbar

--- Website/index.php
+++ Website/index.php
@@ -3,44 +3,63 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BIT FCM | HOME</title>
-    <link rel="stylesheet" href="css/team.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link rel="icon" type="image/png" href="images/bit-fcm.png">
+    <title>HOME | BIT FCM</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
+<body>
 
-<body class="body">
+<nav>
+    <ul>
+        <li><img src="images/bit-fcm.png" alt="" width="130" height="130"></li>
+        <li><a href="#home">HOME</a></li>
+        <li><a href="#events">EVENTS</a></li>
+        <li><a href="#ideas">IDEAS</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+        <li><a href="#team">TEAM</a></li>
+    </ul>
+</nav>
 
-<nav class="stroke navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
+<div class="section" id="home">
+    <h1 class="text">HOME</h1>
+</div>
 
-            <a class="navbar-brand" href="index.php"><img src="images/bit-fcm.png" alt="" width="120" height="120"></a>
+<div class="section" id="events">
+    <h1 class="text">EVENTS</h1>
+</div>
 
-            <button class="navbar-toggler d-flex d-lg-none flex-column justify-content-around" type="button"
-                data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true"
-                aria-label="Toggle navigation">
-                <span class="toggler-icon top-bar"></span>
-                <span class="toggler-icon middle-bar"></span>
-                <span class="toggler-icon bottom-bar"></span>
-            </button>
+<div class="section" id="ideas">
+    <h1 class="text">IDEAS</h1>
+</div>
 
-            <div class="navbar-collapse collapse show float-right" id="navbarNav">
-                <ul class="d-inline navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a href="event.php">EVENT</a></li>
-                    <li class="nav-item"><a href="ideas.php">IDEAS</a></li>
-                    <li class="nav-item"><a href="contact.php">CONTACT</a></li>
-                    <li class="nav-item"><a href="team.php">TEAM</a></li>
-                </ul>
-            </div>
+<div class="section" id="contact">
+    <h1 class="text">CONTACT</h1>
+</div>
 
-        </div>
-    </nav>
+<div class="section" id="team">
+    <h1 class="text">TEAM</h1>
+    <h5 class="text">
+        We are a group of students who want to make the best out of our time at Bit Academy.
+        <br>
+        We had the idea to create an event/party committee to organize events for the other students.
+        <br>
+        We are a group of students who are responsible for the organization of the events.
+        <br>
+        We hope that everyone will enjoy this as much as we do.
+    </h5>
+
+    <h1 class="text">Here Goes Team Image</h1>
+
+    <h5 class="text">
+        We call ourself Bit Mafia, the most dangerous group of Bit Academy.
+        <br>
+        Just kidding, we are just here to organize events for the other students (and to have fun).
+        <br>
+        Our team consists of 4 members, Dylan (3rd year), Felix (2nd year), Chelsea (1st year) and Mehdi (1st year).
+    </h5>
+</div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
-        crossorigin="anonymous"></script>
-    
+
+<script src="js/script.js"></script>
 </body>
 </html>
