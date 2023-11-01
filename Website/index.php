3d7c30553370d5ee5622eff13de1c1ac2458040f
Felix <felix.huel6@gmail.com>
2023-11-01 15:14:49 +0100

    change section

--- Website/index.php
+++ Website/index.php
@@ -12,27 +12,15 @@
     <ul>
         <li><img src="images/bit-fcm.png" alt="" width="130" height="130"></li>
         <li><a href="#home">HOME</a></li>
+        <li><a href="#team">TEAM</a></li>
         <li><a href="#events">EVENTS</a></li>
         <li><a href="#ideas">IDEAS</a></li>
         <li><a href="#contact">CONTACT</a></li>
-        <li><a href="#team">TEAM</a></li>
     </ul>
 </nav>
 
 <div class="section" id="home">
     <h1 class="text">HOME</h1>
-</div>
-
-<div class="section" id="events">
-    <h1 class="text">EVENTS</h1>
-</div>
-
-<div class="section" id="ideas">
-    <h1 class="text">IDEAS</h1>
-</div>
-
-<div class="section" id="contact">
-    <h1 class="text">CONTACT</h1>
 </div>
 
 <div class="section" id="team">
@@ -58,7 +46,17 @@
     </h5>
 </div>
 
+<div class="section" id="events">
+    <h1 class="text">EVENTS</h1>
+</div>
 
+<div class="section" id="ideas">
+    <h1 class="text">IDEAS</h1>
+</div>
+
+<div class="section" id="contact">
+    <h1 class="text">CONTACT</h1>
+</div>
 
 <script src="js/script.js"></script>
 </body>
