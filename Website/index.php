7211d45dd67bc8eca21b9d957e790a3efc471331
Felix <felix.huel6@gmail.com>
2023-11-02 15:25:50 +0100

    ideas page with form made on html, styling of both forms edited and for the table aswell

--- Website/index.php
+++ Website/index.php
@@ -20,42 +20,68 @@
 </nav>
 
 <div class="section" id="home">
-    <h1 class="text">HOME</h1>
+    <h1>HOME</h1>
 </div>
 
 <div class="section" id="team">
-    <h1 class="text">TEAM</h1>
+    <h1>TEAM</h1>
     <h5 class="text">
-        We are a group of students who want to make the best out of our time at Bit Academy.
+        Wij zijn een groepje studenten die de beste tijd uit de opleiding willen halen.
         <br>
-        We had the idea to create an event/party committee to organize events for the other students.
+        Het idee is ontstaan omdat het ons leuk leek om verschillende uitjes en feestjes met de opleiding te doen.
         <br>
-        We are a group of students who are responsible for the organization of the events.
+        We zullen alles organiseren, ieder uitje en feestje.
         <br>
-        We hope that everyone will enjoy this as much as we do.
+        Wij hopen dat jullie het net zo leuk als ons gaan vinden!
     </h5>
 
     <h1 class="text">Here Goes Team Image</h1>
 
     <h5 class="text">
-        We call ourself Bit Mafia, the most dangerous group of Bit Academy.
+        We noemen ons Bit Mafia, de meest gevaarlijke groep van Bit Academy.
         <br>
-        Just kidding, we are just here to organize events for the other students (and to have fun).
+        Grapje! Wij zijn er om alles te organiseren en voor de gezeligheid.
         <br>
-        Our team consists of 4 members, Dylan (3rd year), Felix (2nd year), Chelsea (1st year) and Mehdi (1st year).
+        Wij zijn Felix, Chelsea, Mehdi en Dylan; het hart van de Bit feestcommissie.
     </h5>
 </div>
 
 <div class="section" id="events">
-    <h1 class="text">EVENTS</h1>
+    <h1>EVENTS</h1>
 </div>
 
 <div class="section" id="ideas">
-    <h1 class="text">IDEAS</h1>
+    <h1>IDEAS</h1>
 </div>
 
 <div class="section" id="contact">
-    <h1 class="text">CONTACT</h1>
+    <h1>CONTACT</h1>
+    <div class="container-contact">
+            <div class="contact-info">
+                <table>
+                    <tr>
+                        <th>Name:</th>
+                        <td>BIT FCM</td>
+                    </tr>
+                    <tr>
+                        <th>Email:</th>
+                        <td><a href="mailto:bitfcm23@gmail.com" class="link">bitfcm23@gmail.com</a></td>
+                    </tr>
+                </table>
+            </div>
+        
+            <div class="contact-form">
+                <form method="post">
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" placeholder="Name" required>
+                    <label for="email">Email:</label>
+                    <input type="tel" name="phone" placeholder="Phone" required>
+                    <label for="message">Message:</label>
+                    <textarea name="message" placeholder="Message" required></textarea>
+                    <input type="submit" value="SEND" name="add">
+                </form>
+            </div>
+        </div>
 </div>
 
 <script src="js/script.js"></script>
